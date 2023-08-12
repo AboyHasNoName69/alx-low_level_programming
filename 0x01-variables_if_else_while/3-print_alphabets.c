bec0a4a8c4af7f781dd0e9af2829d002890a7cf1
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-11 17:39:33 -0700

    01 C task 3 commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabets in lower cases, and then in upper
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
