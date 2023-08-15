137e08a171cb1b3f3c6dace3024b52e57e2a8133
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-15 07:55:14 -0700

    02 C task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a string followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	_putchar("0-putchar.c\n");
+return (0);
+}
