bd7921d1363273722b4eb4420e9702c595210621
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-15 16:23:13 -0700

    02 C task 1 commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,14 @@
 /* betty style doc for function main goes there */
 
 void print_alphabet(void);
-{
+(
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-	{
+
 		_putchar(i);
-	}
+
 	_putchar('\n');
-}
+	)
 
 
