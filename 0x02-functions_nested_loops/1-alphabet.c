7029211cbd3946a440fe87817a4902eb869b36b8
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-15 14:26:55 -0700

    02 C task 1 commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,12 @@
 /**
  * main - A program that prints alphabets in lower case followed by a new line
  *
- * Return: 0 (Success)
  */
  void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++);
-		_putchar(letter);
+	_putchar(letter);
 	_putchar('\n');
-	return (0);
 }
