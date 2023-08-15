8a3b77671d82f27b2f9543d710eec73f34ea803b
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-15 12:16:29 -0700

    02 C task 0 commit

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Test the code
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = _abs(-1);
+	printf("%d\n", i);
+	r = _abs(0);
+	printf("%d\n", i);
+	r = _abs(1);
+	printf("%d\n", i);
+	r = _abs(-98);
+	printf("%d\n", i);
+	return (0);
+}
