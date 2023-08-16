eb920d4ae4944be43b4aae2d29854689b3d4025f
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-15 19:56:29 -0700

    02 C task 3 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - is a function that checks if a character is lowercase.
+ * c: is the character to be checked.
+ *
+ * Return: 1 if character is lower case, 0 otherwise.
+ */
+int _islower(int c)
+{
+	int c = 'c';
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
