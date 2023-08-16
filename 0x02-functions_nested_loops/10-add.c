a708b7ea67b7a11772a8378020153fb802208259
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-16 14:34:55 -0700

    02 C task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - function that sums up two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: return 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
