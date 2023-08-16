4e9dd73f483b71c5cf46015d5558d941cc0c569d
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-15 18:16:47 -0700

    02 C task 2 commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - A program that prints alphabets 10 times in lower cases
+ * print_alphabet_x10 - prints alphabets 10 times in lower cases
  * followed by a new line
  *
  * Return: void.
