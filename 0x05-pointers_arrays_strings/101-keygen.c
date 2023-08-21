b14f25ac4f21a913af6db6aa909a77239d1f0ffd
AboyHasNoName69 <nobodysmail69@proton.me>
2023-08-21 14:51:00 -0700

    05 C advanced task 11 commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that generates random valid passwords,
+ * for the program 101-crackme
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int h[100];
+	int i;
+	int sum;
+	int n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		h[i] = rand() % 78;
+		sum += (h[i] + '0');
+		putchar(h[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
